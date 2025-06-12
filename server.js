37a2fd4d29278af173d6a6b110d044e22aa65c01
ofcparvej <ofcparvej@gmail.com>
2025-06-13 01:52:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+var cors = require('cors')
+
+const app = express();
+require('dotenv').config();     
+
+const PORT=process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cors());
+
+require("./config/dataBase").connect();
+
+const user = require("./Routes/User");
+const cart=require("./Routes/Cart");
+const order=require("./Routes/Order");
+const wishlist=require("./Routes/Wishlist");
+const paymentRoute=require("./Routes/Payment")
+app.use("/api/auth",user);
+app.use("/api/cart",cart);
+app.use("/api/order",order);
+app.use("/api/wishlist",wishlist);
+app.use("/api/payment",paymentRoute);
+
+
+
+
+
+ 
+
+
+
+app.listen(PORT , ()=>{
+    console.log(`app is listening at po ${PORT} `)
+})
+
+
+
+
+
